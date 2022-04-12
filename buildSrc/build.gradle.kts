e1dd7aecb4bf796ff3dfea2eabb0c9bd9a164ceb
wrbyepct <wrbyepct11@gmail.com>
2022-04-13 00:08:49 +0800

    Initial commit, successfully built

---
+++ buildSrc/build.gradle.kts
@@ -0,0 +1,15 @@
+import org.gradle.kotlin.dsl.`kotlin-dsl`
+
+repositories {
+    mavenCentral()
+}
+
+plugins {
+    `kotlin-dsl`
+}
+
+/*
+   Why use it?
+      1. It's convenient because it uses kotlin (how?)
+      2. type safety (?)
+ */
